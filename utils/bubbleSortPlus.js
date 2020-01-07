9760700ff69e5db4d3fffecc5722e53cbc749d9a
minghui.zhou <minghui.zhou@jointwisdom.cn>
2020-01-07 11:42:16 +0800

    更新冒泡排序进阶版

--- utils/bubbleSortPlus.js
+++ utils/bubbleSortPlus.js
@@ -1,7 +1,7 @@
 /**
  * @file bubbleSortPlus.js
  * @author bikedawuwang
- * @description 冒泡排序Plus版本
+ * @description 冒泡排序Plus版本  循环次数少10%左右，数据量大能看出差异
 */
 
 let array = [];
@@ -13,7 +13,25 @@ let startTime, endTime;
 
 function bubbleSortPlus(arr) {
     let len = arr.length;
+    startTime = Date.parse(new Date());
     if (!len) {
         return [];
     }
-} 
+    for (let i = 0; i < len; i++) {
+    let point = true; // 如果在一轮比较中没有出现需要交互的数据，说明数组已经有序。
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                point = false;
+            }
+        }
+        endTime = Date.parse(new Date());
+        // console.log(point);
+        console.log(`第${i+1}次循环`, arr);
+        if (point) return;
+    }
+    return arr;
+}
+
+bubbleSortPlus(array);
+console.log("TCL: 共计用时：", `${(endTime - startTime) / 1000}秒`);
